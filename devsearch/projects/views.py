ae38141a6c605607d2b30c678919894bbc5cad3a
kushal <kushal@darwindigital.com>
2022-12-05 15:21:39 +0530

    databse queries

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,7 +1,7 @@
 from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 
 projectList = [
     {
@@ -22,14 +22,10 @@ projectList = [
 ]
 # Create your views here.
 def projects(request):
-    page = "projects"
-    number = 10
-    context = {'page':page, 'number': number, 'projects':projectList}
+    project = Project.objects.all()
+    context = {'projects':project}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None 
-    for i in projectList:
-        if i['id'] == int(pk):
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
